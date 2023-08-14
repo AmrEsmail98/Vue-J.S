b9e17bd2582ca0cdf7beecb02788b1c6c04ad427
Amr Email <amroesmail98@gmail.com>
2023-08-15 00:51:50 +0300

    how to create events

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
-const {createApp} = Vue;
+const { createApp } = Vue;
 
 createApp({
 
-data() {
-    return {
-        name:'Amr Esamil',
-        age:'25',
-        link:"https://www.google.com/"
-    }
-},
-methods: {
-    sayhi($time,$name){
-        return "Good" + " " + $time + ","+"  "+ $name ;
+    data() {
+        return {
+            name: 'Amr Esamil',
+            age: '25',
+            link: "https://www.google.com/"
+        }
+    },
+    methods: {
+        sayhi($time, $name) {
+            return "Good" + " " + $time + "," + "  " + $name;
 
-    }
-},
+        }
+    },
 
 }).mount("#app-root")
+
+
+createApp({
+
+    data() {
+        return {
+            number:0
+        }
+    },
+    methods: {
+        functionIncress(){
+        this.number ++;  
+          },
+          functionDecress(){
+            this.number --;  
+              }
+    
+    },
+
+}).mount("#event")
