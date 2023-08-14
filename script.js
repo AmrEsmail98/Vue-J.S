cbbf65960508bf9156cd9abc4028cab1f3b4a4c1
Amr Email <amroesmail98@gmail.com>
2023-08-15 00:14:57 +0300

    function and binding

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ data() {
     return {
         name:'Amr Esamil',
         age:'25',
+        link:"https://www.google.com/"
+    }
+},
+methods: {
+    sayhi($time,$name){
+        return "Good" + " " + $time + ","+"  "+ $name ;
+
     }
 },
 
